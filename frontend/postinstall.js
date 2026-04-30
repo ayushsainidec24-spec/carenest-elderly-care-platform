89fd5104e525237e4e3db5366c1b9d521dde3a67
ayushsainidec24 spec <ayush.sainidec24@gmail.com>
2026-04-30 23:27:01 +0530

    Fix Babel optional chaining plugin error for Vercel deployment

--- frontend/postinstall.js
+++ frontend/postinstall.js
@@ -62,6 +62,7 @@ if (fs.existsSync(babelCreateFile)) {
     content = content.replace(/@babel\/plugin-proposal-private-methods/g, '@babel/plugin-transform-private-methods');
     content = content.replace(/@babel\/plugin-proposal-private-property-in-object/g, '@babel/plugin-transform-private-property-in-object');
     content = content.replace(/@babel\/plugin-proposal-numeric-separator/g, '@babel/plugin-transform-numeric-separator');
+    content = content.replace(/@babel\/plugin-proposal-optional-chaining/g, '@babel/plugin-transform-optional-chaining');
     // Comment out decorators as it's not available
     if (content.includes('plugin-proposal-decorators') || content.includes('plugin-transform-decorators')) {
       content = content.replace(/isTypeScriptEnabled && \[\s*require\('@babel\/plugin-(?:proposal|transform)-decorators'\)\.default,\s*false,\s*\],/,
